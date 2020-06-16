074d5328836c82150cfe806d12ec3940331781a7
jaypeetan <jaypee.tan@codegum.ph>
2020-06-16 14:29:04 +0800

    Updates

---
+++ precache-manifest.01c1a231283960d2b532700b89f54ab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c49ba9bd3ed99b891faca2965065560c",
+    "url": "/oclock/index.html"
+  },
+  {
+    "revision": "a8444e49c63791e57db6",
+    "url": "/oclock/static/css/main.4e1356e8.chunk.css"
+  },
+  {
+    "revision": "0a244f81191480425042",
+    "url": "/oclock/static/js/2.a627d838.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/oclock/static/js/2.a627d838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8444e49c63791e57db6",
+    "url": "/oclock/static/js/main.9d97b892.chunk.js"
+  },
+  {
+    "revision": "07b684577f9ed1525833",
+    "url": "/oclock/static/js/runtime-main.401a20a7.js"
+  }
+]);
