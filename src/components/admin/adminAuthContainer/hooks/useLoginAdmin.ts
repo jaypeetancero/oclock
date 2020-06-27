97f6901f9e1b365dd1721bbf99e5fc48d61a4ac8
jaypeetan <jaypee.tan@codegum.ph>
2020-06-27 23:07:03 +0800

    added custom hook

--- src/components/admin/adminAuthContainer/hooks/useLoginAdmin.ts
+++ src/components/admin/adminAuthContainer/hooks/useLoginAdmin.ts
@@ -1,21 +1,21 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from "react";
+
+interface AuthProps {
+  username: string;
+  password: string;
+}
 
 interface Props {
-    auth : {
-        username : string;
-        password : string;
-    },
-    companyCode : string;
+  auth: AuthProps;
+  companyCode: string;
 }
 
-export const useLoginAdmin : Props = (props) => {
-    const [status, setStatus] = useState('loading');
-    const [error, setError] = useState('');
-    const [response, setResponse] = useState();
+export const useLoginAdmin = ({ auth, companyCode }: Props) => {
+  const [status, setStatus] = useState("loading");
+  const [error, setError] = useState("");
+  const [response, setResponse] = useState();
 
-
-    return { status, error, response };
-
-}
+  return { status, error, response };
+};
 
 export default useLoginAdmin;
