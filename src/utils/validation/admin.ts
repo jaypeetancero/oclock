9efcb80d2147296fb925f655ef95389a781e046a
jaypeetan <jaypee.tan@codegum.ph>
2020-07-06 21:55:19 +0800

    admin authentication validation

--- src/utils/validation/admin.ts
+++ src/utils/validation/admin.ts
@@ -3,3 +3,8 @@ import * as Yup from "yup";
 export const companyCodeValidationSchema = Yup.object().shape({
   companyCode: Yup.string().required("Required"),
 });
+
+export const authValidationSchema = Yup.object().shape({
+  username: Yup.string().required("Required"),
+  password: Yup.string().required("Required"),
+});
